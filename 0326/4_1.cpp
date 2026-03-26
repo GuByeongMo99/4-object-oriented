76c358debf47b244a376e6e089e96da0190372f2
Gu Byeong Mo <kbmbaksa@naver.com>
2026-03-26 10:30:45 +0900

    0326_1

--- 0326/4_1.cpp
+++ 0326/4_1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-
+//원형 선언
 void f(char c = ' ', int line = 1);
 
+//함수 구현
 void f(char c, int line) 
 {
     for (int i = 0; i < line; i++) 
@@ -17,7 +18,7 @@ void f(char c, int line)
 
 int main() 
 {
-    f();
-    f('%');
-    f('@', 5);
+    f();  // 한줄에 빈칸을 10개 출력한다.
+    f('%'); //한 줄에 '%'를 10개 출력한다.
+    f('@', 5); // 5 줄에 '@' 문자를 10개 출력한다.
 }
